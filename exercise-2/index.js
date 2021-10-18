d5021a4b1a59214dd5024602792b71eba431c4d7
Rafael Silverio <rafael.silverio@kaffa.com.br>
2021-10-18 04:42:53 -0300

    feat: implments bubbleSort execise

---
+++ exercise-2/index.js
@@ -0,0 +1,17 @@
+const bubbleSort = (array) => {
+    let n = array.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (array[j] > array[j + 1]) {
+                let t = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = t;
+            }
+        }
+    }
+    return array;
+}
+
+const originalArray = [5, 3, 2, 4, 7, 1, 0, 6];
+const ordinateArray = bubbleSort(originalArray);
+console.log(ordinateArray);
